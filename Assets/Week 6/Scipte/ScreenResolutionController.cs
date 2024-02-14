f26da1318ebc440ac4de5a867304c003c47c5dde
Charlotte Fung <fungcha@shernet.sheridancollege.ca>
2024-02-14 17:42:29 -0500

    complete task 4a

--- Assets/Week 6/Scipte/ScreenResolutionController.cs
+++ Assets/Week 6/Scipte/ScreenResolutionController.cs
@@ -3,10 +3,23 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScreenResolutionController : MonoBehaviour
 {
+    public void LoadNextscene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
     public void ChangeResolution1()
+    {
+        Screen.SetResolution(1920, 1200, FullScreenMode.Windowed);
+    }
+
+    public void ChangeResolution2()
     {
         Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
     }
