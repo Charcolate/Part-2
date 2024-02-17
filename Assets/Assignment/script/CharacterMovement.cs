e44063e0bb2182805d011337ff761f5da86cd336
Charlotte Fung <fungcha@shernet.sheridancollege.ca>
2024-02-17 17:36:03 -0500

    animation complete

---
+++ Assets/Assignment/script/CharacterMovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 destination;
+    Vector2 movement;
+    public float speed = 3;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        movement = destination - (Vector2)transform.position;
+        if (movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+}
